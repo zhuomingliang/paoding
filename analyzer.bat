0252ef7c3874ce1d756510a89d88656ac3ee5e2f
qieqie.wang <qieqie.wang@24aef33b-e027-0410-9228-fb0eab87d6af>
2007-09-13 01:48:04 +0000

    用于查看庖丁分词效果之用。在命令行输入 analyzer.bat ? 或 ./anlyzer.sh ?查看命令行帮助

---
+++ analyzer.bat
@@ -0,0 +1,2 @@
+@echo off
+java -Dpaoding.try.app=analyzer.bat -Dpaoding.try.cmd=analyzer.bat -cp .;paoding-analysis.jar;./lib/commons-logging.jar;./lib/lucene-core-2.2.0.jar;./lib/lucene-highlighter-2.2.0.jar net.paoding.analysis.analyzer.TryPaodingAnalyzer %1 %2 %3
