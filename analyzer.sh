e1989e1183ce7bc9df1166bd99d51dc94af1b9e6
qieqie.wang <qieqie.wang@24aef33b-e027-0410-9228-fb0eab87d6af>
2007-09-14 02:28:41 +0000

    增加特性：通过--analyzer或-a支持体验其他分词器

--- analyzer.sh
+++ analyzer.sh
@@ -1,1 +1,1 @@
-java -Dpaoding.try.app=./analyzer.sh -Dpaoding.try.cmd=./analyzer.sh  -cp .:paoding-analysis.jar:./lib/commons-logging.jar:./lib/lucene-core-2.2.0.jar:./lib/lucene-highlighter-2.2.0.jar net.paoding.analysis.analyzer.TryPaodingAnalyzer $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -Dpaoding.try.app=./analyzer.sh -Dpaoding.try.cmd=./analyzer.sh  -cp .:paoding-analysis.jar:./lib/commons-logging.jar:./lib/lucene-core-2.2.0.jar:./lib/lucene-analyzers-2.2.0.jar:./lib/lucene-highlighter-2.2.0.jar net.paoding.analysis.analyzer.TryPaodingAnalyzer $1 $2 $3 $4 $5 $6 $7 $8 $9
--- src/net/paoding/analysis/analyzer/TryPaodingAnalyzer.java
+++ src/net/paoding/analysis/analyzer/TryPaodingAnalyzer.java
@@ -6,12 +6,15 @@ import java.io.InputStreamReader;
 
 import net.paoding.analysis.knife.PaodingMaker;
 
+import org.apache.lucene.analysis.Analyzer;
+
 public class TryPaodingAnalyzer {
 	private static final String ARGS_TIP = ":";
 	static String input = null;
 	static String file = null;
 	static String charset = null;
 	static String mode = null;
+	static String analyzerName = null;
 	static String properties = PaodingMaker.DEFAULT_PROPERTIES_PATH;
 	
 	public static void main(String[] args) {
@@ -28,6 +31,7 @@ public class TryPaodingAnalyzer {
 		file = null;
 		charset = null;
 		mode = null;
+		analyzerName = null;
 		properties = PaodingMaker.DEFAULT_PROPERTIES_PATH;
 		
 		int inInput = 0;
@@ -43,6 +47,8 @@ public class TryPaodingAnalyzer {
 				mode = args[++i];
 			} else if (args[i].equals("--properties") || args[i].equals("-p")) {
 				properties = args[++i];
+			} else if (args[i].equals("--analyzer") || args[i].equals("-a")) {
+				analyzerName = args[++i];
 			} else if (args[i].equals("--input") || args[i].equals("-i")) {
 				inInput++;
 			} else if (args[i].equals("--help") || args[i].equals("-h")
@@ -61,6 +67,7 @@ public class TryPaodingAnalyzer {
 				System.out.println("\t--properties, -p:\n\t\t不读取默认的类路径下的庖丁分词属性文件，而使用指定的文件，在前缀加上\"classpath:\"表示从类路径中寻找该文件。");
 				System.out.println("\t--mode, -m:\n\t\t强制使用给定的mode的分词器；可以设定为default,max或指定类名的其他mode(指定类名的，需要加前缀\"class:\")。");
 				System.out.println("\t--input, -i:\n\t\t要被分词的文章内容；当没有通过-f或--file指定文章输入文件时可选择这个选项指定要被分词的内容。");
+				System.out.println("\t--analyzer, -a:\n\t\t测试其他分词器，通过--analyzer或-a指定其完整类名。特别地，paoding、cjk、chinese分别对应PaodingAnalyzer、CJKAnalyzer，ChineseAnalyzer");
 				System.out.println("\n示例:");
 				System.out.println("\t" + cmd + " -h");
 				System.out.println("\t" + cmd + " 中华人民共和国");
@@ -87,9 +94,27 @@ public class TryPaodingAnalyzer {
 	}
 	
 	private static void analysing() throws Exception {
-		PaodingAnalyzer analyzer = new PaodingAnalyzer(properties);
-		if (mode != null) {
-			analyzer.setMode(mode);
+		Analyzer analyzer;
+		if (analyzerName == null || analyzerName.length() == 0 || analyzerName.equalsIgnoreCase("paoding")) {
+			//properties==null等同于new new PaodingAnalyzer();
+			analyzer = new PaodingAnalyzer(properties);
+			if (mode != null) {
+				((PaodingAnalyzer) analyzer).setMode(mode);
+			}
+		}
+		else {
+			Class clz;
+			if (analyzerName.equalsIgnoreCase("standard")) {
+				analyzerName = "org.apache.lucene.analysis.standard.StandardAnalyzer";
+			}
+			else if (analyzerName.equalsIgnoreCase("cjk")) {
+				analyzerName = "org.apache.lucene.analysis.cjk.CJKAnalyzer";
+			}
+			else if (analyzerName.equalsIgnoreCase("cn") || analyzerName.equalsIgnoreCase("chinese")) {
+				analyzerName = "org.apache.lucene.analysis.cn.ChineseAnalyzer";
+			}
+			clz = Class.forName(analyzerName);
+			analyzer = (Analyzer) clz.newInstance();
 		}
 		boolean readInputFromConsle = false;
 		Estimate estimate = new Estimate(analyzer);
@@ -125,7 +150,7 @@ public class TryPaodingAnalyzer {
 		System.out.println("直接输入或粘贴要被分词的内容，以分号;结束，回车后开始分词。");
 		System.out.println("另起一行输入:clear或:c，使此次输入无效，用以重新输入。");
 		System.out.println("要使用命令行参数读入文件内容或其他参数请以冒号:开始，然后输入参数选项。");
-		System.out.println("退出，请输入:exit或:quit");
+		System.out.println("退出，请输入:quit或:q、:exit、:e");
 		System.out.println("需要帮助，请输入:?");
 		System.out.println("注意：指定对文件分词之前要了解该文件的编码，如果系统编码和文件编码不一致，要通过-c指定文件的编码。");
 		System.out.println();
@@ -143,7 +168,7 @@ public class TryPaodingAnalyzer {
 				System.out.println("paoding> Cleared");
 				return getInputFromConsole();
 			}
-			else if (line.equals(ARGS_TIP + "exit") || line.equals(ARGS_TIP + "quit") ) {
+			else if (line.equals(ARGS_TIP + "exit") || line.equals(ARGS_TIP + "quit") || line.equals(ARGS_TIP + "e") || line.equals(ARGS_TIP + "q") ) {
 				System.out.println("Bye!");
 				System.exit(0);
 			}
